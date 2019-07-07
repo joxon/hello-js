c65ac4045adc6d6b4205ac287ed11332fb3ce3d2
Joxon <joxonchan@qq.com>
2019-07-07 20:51:40 +0800

    Updated leetcode 237

---
+++ leetcode/0237. Delete Node in a Linked List copy.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+// Write a function to delete a node
+// (except the tail) in a singly linked list,
+// !! given only access to that node.
+// 不能访问上一个节点
+
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+
+var deleteNode = function(node) {
+  node.val = node.next.val
+  node.next = node.next.next
+}
+
+function ListNode(val) {
+  this.val = val
+  this.next = null
+}
+
+let head = new ListNode(1)
+head.next = new ListNode(2)
+head.next.next = new ListNode(3)
+head.next.next.next = new ListNode(4)
+
+console.log(head)
+deleteNode(head.next)
+console.log(head)
