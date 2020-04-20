51baaf4ca4379b2664f5201109d404919f1aa02d
Junxian Chen <junxian.chen@uci.edu>
2020-04-20 16:55:06 -0700

    lc 1008

---
+++ leetcode/1008. Construct Binary Search Tree from Preorder Traversal.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @return {TreeNode}
+ */
+var bstFromPreorder = function (preorder) {
+  if (!preorder.length) return null;
+
+  const [root, ...rest] = preorder;
+  const rootNode = new TreeNode(root);
+  rootNode.left = bstFromPreorder(rest.filter((n) => n < root));
+  rootNode.right = bstFromPreorder(rest.filter((n) => n > root));
+
+  return rootNode;
+};
